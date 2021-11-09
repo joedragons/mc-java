97206fe2739f5dad310710bf14299f2b78d84ebc
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2021-11-09 21:26:32 +0200

    Bump `model-compiler`

--- version.gradle.kts
+++ version.gradle.kts
@@ -26,7 +26,7 @@
 
 val spineBaseVersion by extra("2.0.0-SNAPSHOT.74")
 val toolBaseVersion by extra("2.0.0-SNAPSHOT.74")
-val mcVersion by extra("2.0.0-SNAPSHOT.75")
+val mcVersion by extra("2.0.0-SNAPSHOT.76")
 
 val mcJavaVersion by extra("2.0.0-SNAPSHOT.75")
 val versionToPublish by extra(mcJavaVersion)
