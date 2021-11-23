3aabf9d5d8dd3de774bb22cd9c563f1384aa2d84
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2021-11-23 22:30:49 +0200

    Put annotation jobs to the package level

---
+++ mc-java-annotation/src/main/java/io/spine/tools/mc/java/annotation/mark/PatternJob.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2021, TeamDev. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.tools.mc.java.annotation.mark;
+
+import io.spine.code.java.ClassName;
+
+/**
+ * An annotation {@link Job} which covers generated Java classes which have a
+ * certain naming.
+ *
+ * <p>For example, all classes ending with {@code OrBuilder}.
+ */
+final class PatternJob extends AnnotationJob {
+
+    private final ClassNamePattern pattern;
+
+    PatternJob(ClassNamePattern pattern, ClassName annotation) {
+        super(annotation);
+        this.pattern = pattern;
+    }
+
+    @Override
+    public void execute(AnnotatorFactory factory) {
+        ClassName annotation = annotation();
+        ClassNamePattern pattern = this.pattern;
+        _debug().log("Annotating classes matching `%s` with `%s`.", pattern, annotation);
+        factory.createPatternAnnotator(annotation, pattern)
+               .annotate();
+        _debug().log("Pattern `%s` processed.", pattern);
+    }
+}
