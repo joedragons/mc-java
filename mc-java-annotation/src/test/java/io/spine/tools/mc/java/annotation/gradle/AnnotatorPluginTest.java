40f350aa2a9aa4fce9cfa8466b705b1422cf982c
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2021-12-01 12:18:22 +0200

    Improve names

--- mc-java-annotation/src/test/java/io/spine/tools/mc/java/annotation/gradle/AnnotatorPluginTest.java
+++ mc-java-annotation/src/test/java/io/spine/tools/mc/java/annotation/gradle/AnnotatorPluginTest.java
@@ -83,15 +83,15 @@ import static org.gradle.api.tasks.SourceSet.MAIN_SOURCE_SET_NAME;
 @DisplayName("`AnnotatorPlugin` should")
 class AnnotatorPluginTest {
 
-    private static final String PROJECT_NAME = "annotator-plugin-test";
+    private static final String RESOURCE_DIR = "annotator-plugin-test";
 
-    private static File testProjectDir = null;
+    private static File projectDir = null;
 
     @BeforeAll
     static void compileProject() {
-        testProjectDir = TempDir.forClass(AnnotatorPluginTest.class);
-        GradleProject project = GradleProject.setupAt(testProjectDir)
-                .fromResources(PROJECT_NAME)
+        projectDir = TempDir.forClass(AnnotatorPluginTest.class);
+        GradleProject project = GradleProject.setupAt(projectDir)
+                .fromResources(RESOURCE_DIR)
                 .copyBuildSrc()
                 .create();
         project.executeTask(annotateProto);
@@ -208,7 +208,7 @@ class AnnotatorPluginTest {
     void compilingSources() {
         File tempDir = TempDir.forClass(AnnotatorPluginTest.class);
         GradleProject project = GradleProject.setupAt(tempDir)
-                .fromResources(PROJECT_NAME)
+                .fromResources(RESOURCE_DIR)
                 .create();
         project.executeTask(compileJava);
     }
@@ -275,7 +275,7 @@ class AnnotatorPluginTest {
     }
 
     private static void check(Path sourcePath, SourceCheck check) throws IOException {
-        Path filePath = DefaultJavaPaths.at(testProjectDir)
+        Path filePath = DefaultJavaPaths.at(projectDir)
                                         .generated()
                                         .mainJava()
                                         .resolve(sourcePath);
@@ -287,7 +287,7 @@ class AnnotatorPluginTest {
 
     private static void checkGrpcService(SourceFile serviceFile, SourceCheck check)
             throws IOException {
-        Path fullPath = DefaultJavaPaths.at(testProjectDir)
+        Path fullPath = DefaultJavaPaths.at(projectDir)
                                         .generated()
                                         .mainGrpc()
                                         .resolve(serviceFile);
@@ -311,10 +311,10 @@ class AnnotatorPluginTest {
      * as defined in the test project under {@code resources/annotator-plugin-test}.
      */
     private static Path mainDescriptorPath() {
-        return DefaultJavaPaths.at(testProjectDir)
+        return DefaultJavaPaths.at(projectDir)
                 .buildRoot()
                 .descriptors()
                 .forSourceSet(MAIN_SOURCE_SET_NAME)
-                .resolve("io.spine.test_" + testProjectDir.getName() + "_3.14" + DESC_EXTENSION);
+                .resolve("io.spine.test_" + projectDir.getName() + "_3.14" + DESC_EXTENSION);
     }
 }
