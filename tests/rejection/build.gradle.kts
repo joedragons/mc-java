d7ae65cccdea86ca92e4496a54390ec00d6935ed
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2021-12-14 14:50:00 +0200

    Remove dead code

--- tests/rejection/build.gradle.kts
+++ tests/rejection/build.gradle.kts
@@ -23,12 +23,3 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
-sourceSets {
-    main {
-        java.srcDirs("$buildDir/generated-proto/main/spine", "$buildDir/generated-proto/main/grpc")
-    }
-    test {
-        java.srcDirs("$buildDir/generated-proto/test/spine", "$buildDir/generated-proto/test/grpc")
-    }
-}
