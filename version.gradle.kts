a533abc3fc60f8630bc50f9f61ea603b1260b676
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2022-01-10 18:16:44 +0200

    Bump version

--- version.gradle.kts
+++ version.gradle.kts
@@ -36,5 +36,5 @@ val mcVersion by extra("2.0.0-SNAPSHOT.87")
  */
 val validationVersion by extra("2.0.0-SNAPSHOT.12")
 
-val mcJavaVersion by extra("2.0.0-SNAPSHOT.88")
+val mcJavaVersion by extra("2.0.0-SNAPSHOT.89")
 val versionToPublish by extra(mcJavaVersion)
