73397615220176de634a23d553a1db7f9b0e5246
Alex Tymchenko <alex.tymchenko@teamdev.com>
2022-07-11 13:48:31 +0300

    Remove the now-unused reference to the Gradle plugin portal.

--- mc-java-validation/src/test/resources/validation-gen-plugin-test/build.gradle.kts
+++ mc-java-validation/src/test/resources/validation-gen-plugin-test/build.gradle.kts
@@ -72,8 +72,6 @@ group = "io.spine.test"
 version = "3.14"
 
 repositories {
-    // Some Gradle plugins are needed to be resolved in scope of `protoData` configuration.
-    gradlePluginPortal()
     mavenLocal()
     mavenCentral()
     maven { url = uri(Repos.artifactRegistry) }
