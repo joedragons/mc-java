b41fc3461d0a20bb4c38ade33ace9d722a5231bb
Alex Tymchenko <alex.tymchenko@teamdev.com>
2022-08-29 18:32:29 +0300

    Remove an extra expression.

--- mc-java-annotation/src/test/resources/annotator-plugin-test/build.gradle.kts
+++ mc-java-annotation/src/test/resources/annotator-plugin-test/build.gradle.kts
@@ -49,7 +49,6 @@ buildscript {
             exclude(group = "com.google.guava")
         }
         classpath("io.spine.tools:spine-mc-java-plugins:${mcJavaVersion}:all")
-        classpath("io.spine.tools:spine-mc-java-plugins:${mcJavaVersion}:all")
         classpath("io.spine:protodata:${protoDataVersion}")
     }
 }
